8a1721a21d7c6d4be55f7b50c08431837d26e3a4
Aleksey Zakharov <ayza@yandex-team.ru>
2019-09-27 13:03:29 +0300

    Corrected kms endpoint

--- kms-client.sh
+++ kms-client.sh
@@ -4,10 +4,10 @@ if [[ "$DEBUG" == 1 ]]; then
   set -x
 fi
 
-IAM_TOKEN=`yc iam create-token`
-KMS_HTTP_ENDPOINT=${KMS_HTTP_ENDPOINT:-"kms.yandex:443"}
+IAM_TOKEN=${IAM_TOKEN:-`yc iam create-token`}
+KMS_HTTP_ENDPOINT=${KMS_HTTP_ENDPOINT:-"https://kms.yandex:443"}
 
-curl -s --insecure \
+curl -s \
     -H "Authorization: Bearer ${IAM_TOKEN}" \
     -H "Content-type: application/json" \
     --data "${KMS_HTTP_REQUEST}" \
